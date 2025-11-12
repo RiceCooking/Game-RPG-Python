61dcf23499bbdd233f0f54f62ec05b179d99ebbd
Haekal <andihaekal338@gmail.com>
2025-11-12 20:28:13 +0800

    Fungsi untuk register

--- src/halamanmasuk.py
+++ src/halamanmasuk.py
@@ -1,5 +1,72 @@
+from tinydb import TinyDB, Query
+import time
+import getpass
+
+DB_PATH = 'data/rpg_game_db.json' 
+db = TinyDB(DB_PATH)
+users = db.table('users')
+players = db.table('players')
+user = Query()
+player = Query()
+
 def register():
     print("=== Register ===")
+    try:
+        username = input("Username\t\t:")
+        password = getpass.getpass("Password\t\t:")
+        konfpass = getpass.getpass("Konfirmasi Password\t:")
+
+        if " " in username:
+            print("Username tidak boleh mengandung spasi")
+            time.sleep(2)
+            return
+        if " " in password:
+            print("Password tidak boleh mengandung spasi")
+            time.sleep(2)
+            return     
+        if password != konfpass:
+            print("Password tidak sesuai")
+            time.sleep(2)
+            return
+        if len(username) < 3:
+            print("Username minimal 3 karakter")
+            time.sleep(2)
+            return
+        if len(password) < 3:
+            print("Password minimal 3 karakter")
+            time.sleep(2)
+            return
+        if users.search(user.username == username):
+            print("Username tidak tersedia")
+            time.sleep(2)
+            return
+        
+        id = users.insert({
+            'username' : username,
+            'password' : password
+        })
+
+        players.insert({
+            'user_id' : id,
+            'karakter' : None,
+            'class' : None,
+            'level' : 0,
+            'exp' : 0,
+            'hp' : 0,
+            'attack' : 0,
+            'koin' : 150,
+            'inventory' : [],
+            'senjata_aktif' : None
+        })
+        print("Berhasil Registrasi\n Pengguna baru mendapatkan 150 koin.")
+        time.sleep(3)
+
+    except (KeyboardInterrupt, EOFError):
+        print("Bearlih ke halaman masuk")
+        time.sleep(2)
+        return
+
+        
 
 def login():
     print("=== Login ====")
