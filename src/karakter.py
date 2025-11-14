8916a278970f1aab87265d046fc8aceadad0ba7e
Haekal <andihaekal338@gmail.com>
2025-11-14 12:16:19 +0800

    Strukture dari opsi karakter

---
+++ src/karakter.py
@@ -0,0 +1,36 @@
+#jadi nanti statistik, inventory, dan keluar ini bentuk kerangka utama dari opsi karakter
+def karakter():
+    # TODO
+    #Kerangaka utama isinya perulangan dengan opsi
+    # 1. Statistik
+    # 2. Inventory
+    # 3. Kembali (ke menu utama)
+    pass
+
+def statistik():
+    # TODO 
+    #Nanti bisa lihat Level karakter (exp), attack, hp 
+    pass
+
+def inventory():
+    #TODO
+    #Disini kerangka utama inventory karakter
+    ###
+    # Intiya nanti disini tuh perulangan, dengan opsi
+    # 1. Lihat Senjata 
+    # 2. Hapus Senjata (Note jika senjata hanya satu tidak bisa mengahapus senjata yang ada)
+    # 3. Ganti Senjata Aktif (Ini wajib pakai senjata satu ga bisa ga pakai senjata)
+    # Kembali -> jadi nanti di perulangan di invetory ini berhenti dan kemabli ke menu opsi karakter (statistik, inventory, kembali(ke menu utama))
+    pass
+
+def lihat_senjata():
+    #TODO
+    pass
+
+def hapus_senjata():
+    #TODO
+    pass
+
+def ganti_senjata():
+    #TODO
+    pass
