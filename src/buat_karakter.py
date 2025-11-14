2ccb762abe7c7a831e5a57575153ef84f49d5ac5
Haekal <andihaekal338@gmail.com>
2025-11-14 13:09:39 +0800

    Sturukture dasar fitur buat karakter

---
+++ src/buat_karakter.py
@@ -0,0 +1,7 @@
+def buat_karakter():
+    #TODO
+    #Jadi fitur ini hanya berlaku jika nilai dari 'karakter' (bagian register) = None. Jadi intinya fitur ini otomatis muncul untuk akun baru
+    #-> Ada dua tipe karakter
+	#-> Class 1 : Warrior, HP : 70,  Attack : 100, level : 1
+	#-> Class 2 : Tank, HP : 150, Attack : 65, level : 1
+    pass
