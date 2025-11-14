c410923d19beb7a6f3c22182f3220110761d6acc
Haekal <andihaekal338@gmail.com>
2025-11-14 13:05:44 +0800

    Kerang dasar fitur battle

---
+++ src/battle.py
@@ -0,0 +1,10 @@
+def battle():
+    #TODO
+    #Jadi sebelum masuk battle user akan memilih 3 opsi boss
+    #-> Boss lvl 1 x 1 menang = 50 koin + 50 exp
+	#-> Boss lvl 2 x 1 menang = 75 koin + 75 exp
+	#-> Boss lvl 3 x 1 menang = 150 koin + 150 xp
+
+    #Masuk ke sistem battle nya jadi user akan memulai serangan terlebih dahulu, lalu dilanjut oleh boss yang menyerah. Akan terus seperti itu sampai salah satu pihak hp <= 0.
+    #Jika user menanga maka dapat reward jika kalah tidak mendapatkan apa apa
+    pass
