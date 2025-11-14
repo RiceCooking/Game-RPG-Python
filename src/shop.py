56a956b141a97a2073f6202e36035858245a7cb9
Haekal <andihaekal338@gmail.com>
2025-11-14 13:01:47 +0800

    Kerangka dasar fitur shop

---
+++ src/shop.py
@@ -0,0 +1,11 @@
+def shop():
+    #TODO
+    #Jadi disini tempat gacha, 1 pull 100 koin, 10 pull 1000 koin.
+    #Jadi pilihannya nya di shop
+    # 1. Pull x 1
+    # 2. Pull x 10
+    # 3. Kembali (ke menu utama)
+    # -> Rarity Senjata (Common, Uncommon, Rare, Epic, Legendary, dan Mythical)
+	# -> Jenis senjata : pedang
+	# -> Rarity menentukan bonus attack
+    pass
