7f99fd258c47f5c5167414fc20765b8483b94696
Haekal <andihaekal338@gmail.com>
2025-11-27 02:13:39 +0800

    Main utama program

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-from src.halaman_masuk import halaman_masuk, players, users, loading, clear
+from src.menu import main
 
-sign_in = True
-login_s = None
-
-while sign_in == True:
-    hasil = halaman_masuk()
-    # Kalau user minta KELUAR (return False)
-    if hasil == False:
-        sign_in = False
-    # Kalau user berhasil LOGIN (return Data User)
-    elif hasil is not None:
-        login_s = hasil
-
-    while login_s != None:
-        #TODO
-        pass
+if __name__ == "__main__":
+    main()
